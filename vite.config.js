08fd5b92150f73bfe2f3e02edb09fc3fee7dbcbc
RickRick84 <flsheandoxahi@gmail.com>
2025-05-05 15:27:04 -0500

    Fix vite config sin base y src/main.jsx correcto

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/PokerApp/', // importante para subruta
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
-  server: {
-    port: 5173,
-  },
 });
