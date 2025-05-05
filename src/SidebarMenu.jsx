846e2ddaed2ce84ed46f4975dfd2ae6d5902eeee
RickRick84 <flsheandoxahi@gmail.com>
2025-05-05 11:54:46 -0500

    Corrijo import de logo PokerBet: uso JPG en lugar de SVG

--- src/SidebarMenu.jsx
+++ src/SidebarMenu.jsx
@@ -1,19 +1,18 @@
-// src/SidebarMenu.jsx
 import React from 'react';
 import './SidebarMenu.css';
-import { ReactComponent as PokerBetLogo } from './PokerBetLogo.svg';
+import PokerBetLogo from '/pb_logo.png';
 
 const SidebarMenu = ({ isOpen, toggleSidebar }) => {
   return (
     <div className={`sidebar-menu ${isOpen ? 'open' : ''}`}>
-      <div className="sidebar-logo-box">
-        <PokerBetLogo className="logo" />
+      <div className="sidebar-logo-container">
+        <div className="sidebar-logo-box">
+          <img src={PokerBetLogo} alt="PokerBet" className="sidebar-logo" />
+        </div>
       </div>
-
       <button className="sidebar-link">🧢 MERCHANDISING</button>
       <button className="sidebar-link">🎟️ SORTEO SEMANAL</button>
       <button className="sidebar-link">🎰 SORTEO MENSUAL</button>
-      <button className="sidebar-link">✈️ PAQUETES DE VIAJE</button>
       <button className="sidebar-link">📆 CALENDARIO</button>
       <button className="sidebar-link">📰 NEWS</button>
     </div>
